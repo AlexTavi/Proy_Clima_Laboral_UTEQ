f4c30469f4f343768f1cfd6cbe4de5bb7dc5e2f2
diego <diegoe.aq.03@gmail.com>
2025-07-27 10:44:00 -0600

    Corrección de rutas v1.0.1 7/27/25

--- clima_laboral/backend/routes/api.php
+++ clima_laboral/backend/routes/api.php
@@ -5,25 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CuestionarioController;
 
-// ==============================
-// Rutas públicas (sin autenticación)
-// ==============================
-Route::get('/cuestionarios', [CuestionarioController::class, 'index']);
-Route::post('/cuestionario/edit', [CuestionarioController::class, 'getCuestionarioEdit']);
-Route::put('/update-reactivos/{id_cr}', [CuestionarioController::class, 'update']);
-Route::delete('/delete-reactivos/{id_cr}', [CuestionarioController::class, 'destroyReactivo']);
-
-
+// Rutas públicas
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-
-// ==============================
-// Rutas protegidas (requieren token Sanctum)
-// ==============================
+// Rutas protegidas con Sanctum (requieren token)
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/forms', [EmpresaController::class, 'store']);
     Route::get('/forms', [EmpresaController::class, 'index']);
     Route::post('/destroy/empresa', [EmpresaController::class, 'destroy']);
+    Route::get('/cuestionarios', [CuestionarioController::class, 'index']);
+    Route::post('/cuestionario/edit', [CuestionarioController::class, 'getCuestionarioEdit']);
+    Route::put('/update-reactivos/{id_cr}', [CuestionarioController::class, 'update']);
+    Route::delete('/delete-reactivos/{id_cr}', [CuestionarioController::class, 'destroyReactivo']);
 });
